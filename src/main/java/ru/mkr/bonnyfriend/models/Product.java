3e51fb49edd21d952b4b9454f31a3a44bf2937fd
MKrasnoperov <muxajlu-4@yandex.ru>
2021-11-23 20:44:49 +0400

    added lombok

--- src/main/java/ru/mkr/bonnyfriend/models/Product.java
+++ src/main/java/ru/mkr/bonnyfriend/models/Product.java
@@ -1,10 +1,13 @@
 package ru.mkr.bonnyfriend.models;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.util.Objects;
 
+@Data
 @Entity
 public class Product {
   @Id
@@ -38,75 +41,6 @@ public class Product {
     this.imagePath = image;
     this.recall = recall;
     this.specification = specification;
-  }
-
-  public URI getImagePath() {
-    return imagePath;
-  }
-
-  public void setImagePath(URI imagePath) {
-    this.imagePath = imagePath;
-  }
-
-  public String getRecall() {
-    return recall;
-  }
-
-  public void setRecall(String recall) {
-    this.recall = recall;
-  }
-
-  public String getSpecification() {
-    return specification;
-  }
-
-  public void setSpecification(String specification) {
-    this.specification = specification;
-  }
-
-  public BigDecimal getPrice() {
-    return price;
-  }
-
-  public void setPrice(BigDecimal price) {
-    this.price = price;
-  }
-
-  public BigDecimal getSalePrice() {
-    return salePrice;
-  }
-
-  public void setSalePrice(BigDecimal salePrice) {
-    this.salePrice = salePrice;
-  }
-
-  protected Product(String title, String description) {
-    this.title = title;
-    this.description = description;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
   }
 
   @Override
